5a2cd027197a6045b320f5253a9c1ffed47ee830
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-06-01 17:13:51 -0400

    inti commit

---
+++ src/main/java/org/kras/redismultithreading/RedismultithreadingApplication.java
@@ -0,0 +1,13 @@
+package org.kras.redismultithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedismultithreadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedismultithreadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kras/redismultithreading/RedismultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kras.redismultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedismultithreadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
